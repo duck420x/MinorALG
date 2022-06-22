0d6f7197917c1ea7fbcc84de62c2744ca6da320a
Oliver <oliver.boros.nl@gmail.com>
2022-06-22 09:36:05 +0200

    js etc

--- scripts/einde.js
+++ scripts/einde.js
@@ -1,25 +1,19 @@
 "use strict()";
 
-let num1 = document.getElementById("num1");
-let num2 = document.getElementById("num2");
-let num3 = document.getElementById("num3");
-let num4 = document.getElementById("num4");
-let num5 = document.getElementById("num5");
-let num6 = document.getElementById("num6");
-let num7 = document.getElementById("num7");
-let num8 = document.getElementById("num8");
-let num9 = document.getElementById("num9");
-let num10 = document.getElementById("num10");
+let clicked = 0;
+
+let pin = document.getElementById("pin");
 let submit = document.getElementById("submit");
 
-num1.addEventListener("click", buttonclick(1));
-num2.addEventListener("click", buttonclick(2));
-num3.addEventListener("click", buttonclick(3));
-num4.addEventListener("click", buttonclick(4));
-num5.addEventListener("click", buttonclick(5));
-num6.addEventListener("click", buttonclick(6));
-num7.addEventListener("click", buttonclick(7));
-num8.addEventListener("click", buttonclick(8));
-num9.addEventListener("click", buttonclick(9));
-num10.addEventListener("click", buttonclick(10));
-// submit.addEventListener("click", buttonclick(submit));
+pin.addEventListener("click", buttonClick);
+submit.addEventListener("click", buttonClick);
+
+function buttonClick() {
+	if (clicked <= 4) {
+		clicked++;
+		console.log(clicked);
+	} else {
+		console.error("u got in else loop why?");
+	}
+}
+console.log("loaded");

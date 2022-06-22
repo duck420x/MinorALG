905c5427633f3cbf74af29637038f1fd12ac0d12
Oliver <oliver.boros.nl@gmail.com>
2022-06-22 09:03:04 +0200

    js shit

---
+++ scripts/einde.js
@@ -0,0 +1,25 @@
+"use strict()";
+
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+let num3 = document.getElementById("num3");
+let num4 = document.getElementById("num4");
+let num5 = document.getElementById("num5");
+let num6 = document.getElementById("num6");
+let num7 = document.getElementById("num7");
+let num8 = document.getElementById("num8");
+let num9 = document.getElementById("num9");
+let num10 = document.getElementById("num10");
+let submit = document.getElementById("submit");
+
+num1.addEventListener("click", buttonclick(1));
+num2.addEventListener("click", buttonclick(2));
+num3.addEventListener("click", buttonclick(3));
+num4.addEventListener("click", buttonclick(4));
+num5.addEventListener("click", buttonclick(5));
+num6.addEventListener("click", buttonclick(6));
+num7.addEventListener("click", buttonclick(7));
+num8.addEventListener("click", buttonclick(8));
+num9.addEventListener("click", buttonclick(9));
+num10.addEventListener("click", buttonclick(10));
+// submit.addEventListener("click", buttonclick(submit));

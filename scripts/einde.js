b440c29353e7d7212bb22eb5bac2f05b940ac406
Oliver <oliver.boros.nl@gmail.com>
2022-06-22 09:47:02 +0200

    js shit (alweer)

--- scripts/einde.js
+++ scripts/einde.js
@@ -2,18 +2,40 @@
 
 let clicked = 0;
 
-let pin = document.getElementById("pin");
+let pin1 = document.getElementById("pin1");
+let pin2 = document.getElementById("pin2");
+let pin3 = document.getElementById("pin3");
+let pin4 = document.getElementById("pin4");
+let pin5 = document.getElementById("pin5");
+let pin6 = document.getElementById("pin6");
+let pin7 = document.getElementById("pin7");
+let pin8 = document.getElementById("pin8");
+let pin9 = document.getElementById("pin9");
+let pin10 = document.getElementById("pin10");
 let submit = document.getElementById("submit");
 
-pin.addEventListener("click", buttonClick);
-submit.addEventListener("click", buttonClick);
+pin1.addEventListener("click", pinClick);
+pin2.addEventListener("click", pinClick);
+pin3.addEventListener("click", pinClick);
+pin4.addEventListener("click", pinClick);
+pin5.addEventListener("click", pinClick);
+pin6.addEventListener("click", pinClick);
+pin7.addEventListener("click", pinClick);
+pin8.addEventListener("click", pinClick);
+pin9.addEventListener("click", pinClick);
+pin10.addEventListener("click", pinClick);
+submit.addEventListener("click", submitClick);
 
-function buttonClick() {
-	if (clicked <= 4) {
+function pinClick() {
+	if (clicked < 4) {
 		clicked++;
 		console.log(clicked);
-	} else {
-		console.error("u got in else loop why?");
 	}
 }
-console.log("loaded");
+function submitClick() {
+	if (clicked == 4) {
+        window.location.replace("./index.html");
+	} else {
+		console.error("not enough clicks");
+	}
+}

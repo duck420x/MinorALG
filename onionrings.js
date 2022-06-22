fcf99ed8d0c39234728476a5ac22aacca8b01810
damianzm04 <88124@glr.nl>
2022-06-22 10:41:12 +0200

    alles denk ik
    ja

---
+++ onionrings.js
@@ -0,0 +1,33 @@
+let aantalAppels = 0;
+
+function koopProduct(fruit, prijs)
+{
+    if (fruit == "appel")
+    {
+        aantalAppels++;
+        let prijsAppels = aantalAppels * 3;
+        document.getElementById("totaal_appel").value = aantalAppels;
+        document.getElementById("totaalPrijs_appel").value = prijsAppels;
+    }
+}
+
+function verwijderProduct(fruit, prijs)
+{
+        if (fruit == "appel")
+    {
+        aantalAppels--;
+        if (aantalAppels < 0)
+        {
+            aantalAppels = 0;
+        }
+        let prijsAppels = aantalAppels;
+        document.getElementById("totaal_appel").value = aantalAppels;
+        document.getElementById("totaalPrijs_appel").value = prijsAppels;
+    }
+}
+
+function berekenPrijs()
+{
+    let totaalPrijs = aantalAppels * 3;
+    document.getElementById("totaalPrijs").value = totaalPrijs;
+}
